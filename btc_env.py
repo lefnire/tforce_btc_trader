Scaling: prior fix too slow; pre-scale observations (batch), online-scale rewards/stationary

btc_env.py
@@ -74,23 +74,28 @@ class Scaler(object):
         # Skip every few fittings. Each individual doesn't contribute a whole lot anyway, and costs a lot
         return self.done or (self.i % self.SKIP != 0 and self.i > self.SKIP)
 
-    def transform(self, row, kind):
+    def transform(self, input, kind):
         # this is awkward; we only want to increment once per step, but we're calling this fn 3x per step (once
         # for series, once for stationary, once for reward). Explicitly saying "only increment for one of those" here.
         # Using STATIONARY since SERIES might be called once per timestep in a conv window. TODO Seriously awkward
         if kind == self.STATIONARY: self.i += 1
 
-        scaler, data = self.scalers[kind], self.data[kind]
+        scaler = self.scalers[kind]
+        matrix = np.array(input).ndim == 2
         if self._should_skip():
-            return scaler.transform([row])[-1]
+            if matrix: return scaler.transform(input)
+            return scaler.transform([input])[-1]
         # Fit, transform, return
-        data.append(row)
-        ret = scaler.fit_transform(data)[-1]
-        if kind == self.REWARD: ret = ret[0]
+        data = self.data[kind]
+        if matrix:
+            self.data[kind] += input.tolist()
+            ret = scaler.fit_transform(data)[-input.shape[0]:]
+        else:
+            data.append(input)
+            ret = scaler.fit_transform(data)[-1]
         if self.i >= self.STOP_AT and not self.done:
             self.done = True
-            # Clear up memory, fitted scalers have all the info we need. stop=True only needed in one of these functions
-            del self.data
+            del self.data # Clear up memory, fitted scalers have all the info we need.
         return ret
 
     def avg_reward(self):
@@ -237,7 +242,11 @@ class BitcoinEnv(Environment):
 
         states = np.nan_to_num(np.column_stack(columns))
         prices = df[data.target].values
-        # Note: don't scale/normalize here, since we'll normalize w/ self.price/step_acc.cash after each action
+
+        # Pre-scale all price actions up-front, since they don't change. We'll scale changing values real-time elsewhere
+        if self.hypers.scale:
+            states = self.scaler.transform(states, Scaler.SERIES)
+
         return states, prices
 
     def use_dataset(self, mode, no_kill=False):
@@ -270,26 +279,21 @@ class BitcoinEnv(Environment):
         # print(f"Loading {mode.name} took {after_time}s")
 
     def _get_next_state(self, i, cash, value, repeats):
-        next_series = self.observations[i]
-        next_stationary = [cash, value, repeats]
-        if self.hypers.repeat_last_state:
-            next_stationary += next_series.tolist()
+        series = self.observations[i]
+        stationary = [cash, value, repeats]
         if self.hypers.scale:
-            next_series = self.scaler.transform(next_series, Scaler.SERIES)
-            next_stationary = self.scaler.transform(next_stationary, Scaler.STATIONARY)
+            # series already scaled in self._xform_data()
+            stationary = self.scaler.transform(stationary, Scaler.STATIONARY).tolist()
 
         if self.conv2d:
+            if self.hypers.repeat_last_state:
+                stationary += series.tolist()
             # Take note of the +1 here. LSTM uses a single index [i], which grabs the list's end. Conv uses a window,
             # [-something:i], which _excludes_ the list's end (due to Python indexing). Without this +1, conv would
             # have a 1-step-behind delayed response.
-            window = []
-            for j in range(i - self.hypers.step_window + 1, i):
-                step = self.observations[i]
-                window.append(self.scaler.transform(step, Scaler.SERIES) if self.hypers.scale else step)
-            window.append(next_series)
-            # window = self.observations[i - self.hypers.step_window + 1:i + 1]
-            next_series = np.expand_dims(window, axis=1)
-        return dict(series=next_series, stationary=next_stationary)
+            window = self.observations[i - self.hypers.step_window + 1:i + 1]
+            series = np.expand_dims(window, axis=1)
+        return dict(series=series, stationary=stationary)
 
     def reset(self):
         self.time = time.time()
@@ -364,7 +368,7 @@ class BitcoinEnv(Environment):
 
         next_state = self._get_next_state(step_acc.i, step_acc.cash, step_acc.value, step_acc.repeats)
         if self.hypers.scale:
-            reward = self.scaler.transform([reward], Scaler.REWARD)
+            reward = self.scaler.transform([reward], Scaler.REWARD)[0]
 
         terminal = int(step_acc.i + 1 >= len(self.observations))
         # Kill and punish if (a) agent ran out of money; (b) is doing nothing for way too long
@@ -421,11 +425,7 @@ class BitcoinEnv(Environment):
             if signal != 0:
                 print(f"New Total: {step_acc.cash + step_acc.value}")
                 self.episode_finished(None)  # Fixme refactor, awkward function to call here
-            next_state['stationary'] = [
-                step_acc.cash / self.start_cash,
-                step_acc.value / self.start_value,
-                repeats_scaled  # TODO do I need to handle specifically for live?
-            ]
+            next_state['stationary'] = [step_acc.cash, step_acc.value, self.repeats]
             terminal = False
 
         # if step_acc.value <= 0 or step_acc.cash <= 0: terminal = 1